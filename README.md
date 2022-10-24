c1c2984e04ce401ae034b5758da1b7a62c964124
Raphael Nobre <nobre.roots@gmail.com>
2022-10-24 17:04:25 -0300

    readme e comentários

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Portfólio
 
-Projetos desenvolvidos por mim.
+Portfólio apresentando alguns projetos desenvolvidos por mim
+
+## RocketPay 
+
+Projeto desenvolvido no explorer lab da RocketSeat
+[] JavaScript
+[] DOM manipulation
+[] IMask
+
+## Clone do Google
+
+Projeto para o curso CS50 da Universidade de Harvard
+[] Estudo de Queries
--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-document.getElementById("linkedIn").onclick = function () {
-    window.open("https://www.linkedin.com/in/raphael-brandão-969281225/");
-}
-
-document.getElementById("gitHub").onclick = function () {
-    window.open("https://github.com/raphaelnb");
-}
-
-document.getElementById("orc").onclick = function () {
-    window.open("https://mariaabritta.github.io/SiteProject/#orcestra");
+// Link dos projetos
+document.getElementById("rocketpay").onclick = function () {
+    window.open("https://rocketpay-ignitelab.vercel.app");
 }
 
 document.getElementById("google").onclick = function () {
     window.open("https://raphaelnb.github.io/google/");
 }
 
-document.getElementById("rocketpay").onclick = function () {
-    window.open("https://rocketpay-ignitelab.vercel.app");
+document.getElementById("orc").onclick = function () {
+    window.open("https://mariaabritta.github.io/SiteProject/#orcestra");
+}
+
+// Links das redes sociais
+document.getElementById("linkedIn").onclick = function () {
+    window.open("https://www.linkedin.com/in/raphael-brandão-969281225/");
+}
+
+document.getElementById("gitHub").onclick = function () {
+    window.open("https://github.com/raphaelnb");
 }
